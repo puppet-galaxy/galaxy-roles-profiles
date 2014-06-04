8c8a260ec27a3f536c60a510113b1247f1d0ba5d
root <root@debian-puppet.local>
2014-06-04 14:19:54 +0000

    Modify Hiera parameters.

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -1,31 +1,46 @@
 # == Class: galaxy-roles-profiles::profile::base
 #
-# This class manage basic operations for Galaxy webapp class.
+# This class manage basic operations for profile construction.
+#
+# Parameters set by Hiera:
+# [*galaxy-roles-profiles::profile::create_user*]
+# If enabled allows a new galaxy user creation.
+#
+# [*galaxy-roles-profiles::profile::home_directory*]
+# The full path where the home directory's user will be written.
+#
+# [*galaxy-roles-profiles::profile::app_directory*]
+# The full path where the galaxy repository will be load.
+#
+# [*galaxy-roles-profiles::profile::galaxy_branch*]
+# Which branch of galaxy's development to pull from. Options are "stable" and
+# "default", where default is the primary branch for development work.
 #
 # ==== Examples
 #
-#   if hiera('galaxy-roles-profiles::profile::webapp::core') == 'one'{
+#   if hiera('galaxy-roles-profiles::profile::common::core') == 'one'{
 #    class{ 'galaxy-roles-profiles::profile::base':
 #   }-> ...
 #
 # # === Authors
 #
 # O. Inizan <oinizan@versailles.inra.fr>
-#
+# Eric Rasche <rasche.eric@yandex.ru>
+# S. Bridel <sbridel@versailles.inra.fr>
 # === Copyright
 #
 # Copyright 2014, unless otherwise noted.
 #
 class galaxy-roles-profiles::profile::base(){
   class { 'galaxy':
-    create_user => hiera('galaxy-roles-profiles::profile::webapp::create_user'),
-    directory   => hiera('galaxy-roles-profiles::profile::webapp::home_directory'),
+    create_user => hiera('galaxy-roles-profiles::profile::create_user'),
+    directory   => hiera('galaxy-roles-profiles::profile::home_directory'),
   } ->
   galaxy::install{ 'base':
-    directory     => hiera('galaxy-roles-profiles::profile::webapp::app_directory'),
-    galaxy_branch => hiera('galaxy-roles-profiles::profile::webapp::galaxy_branch'),
+    directory     => hiera('galaxy-roles-profiles::profile::app_directory'),
+    galaxy_branch => hiera('galaxy-roles-profiles::profile::galaxy_branch'),
   } ->
   galaxy::first_run{ 'base':
-    directory   => hiera('galaxy-roles-profiles::profile::webapp::app_directory'),
+    directory   => hiera('galaxy-roles-profiles::profile::app_directory'),
   }
 }
