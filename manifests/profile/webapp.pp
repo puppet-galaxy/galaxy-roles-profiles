1dd55753800f1b4bbea5b529e713ddf74cbc9902
root <root@debian-puppet.local>
2014-06-05 07:41:44 +0000

    first commit

---
+++ manifests/profile/webapp.pp
@@ -0,0 +1,17 @@
+# == Class galaxy-roles-profiles::profile::webapp
+# This class manage web servers configuration.
+#
+# === Examples
+# include galaxy-roles-profiles::profile::webapp
+#
+# === Authors
+#
+# S. Bridel <sbridel@versailles.inra.fr>
+#
+#
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
+#
+class galaxy-roles-profiles::profile::webapp(){
+}
