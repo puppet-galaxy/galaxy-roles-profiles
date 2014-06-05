fa6bc6bcd8fa5b26b3f7fe77388558d0b778582f
root <root@debian-puppet.local>
2014-06-05 07:40:38 +0000

    first commit

--- manifests/role/multicore.pp
+++ manifests/role/multicore.pp
@@ -1,8 +1,21 @@
+# == Class galaxy-roles-profiles::role::basic
+# This role create one instance of galaxy with multicore configuration.
+# That means Galaxy runs with several cores and with SQLite.
 #
+# ==== Examples
+# include galaxy-roles-profiles::role::multicore
+#
+# === Authors
+#
+# O. Inizan <oinizan@versailles.inra.fr>
+# S. Bridel <sbridel@versailles.inra.fr>
+#
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
 class galaxy-roles-profiles::role::multicore(){
-  class{ 'galaxy-role-profiles::profile::common':
-    config => "mutlicore",
-  }
-  class{ 'galaxy-role-profiles::profile::webapp': }	
+  class{ 'galaxy-roles-profiles::profile::common':
+    config => 'multicore',
+  }->
+  class {'galaxy-roles-profiles::profile::webapp':}
 }
-
