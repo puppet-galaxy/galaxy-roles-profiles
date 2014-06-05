4fc4a6012bb238a0f593218211e9b9badfb3e6fd
root <root@debian-puppet.local>
2014-06-05 07:39:17 +0000

    first commit

--- manifests/role/basic.pp
+++ manifests/role/basic.pp
@@ -1,5 +1,19 @@
-#  
+# == Class galaxy-roles-profiles::role::basic
+# This role create one instance of galaxy with basic configuration.
+# That means Galaxy runs with only one core and with SQLite.
+#
+# ==== Examples
+# include galaxy-roles-profiles::role::basic
+#
+# === Authors
+#
+# O. Inizan <oinizan@versailles.inra.fr>
+# S. Bridel <sbridel@versailles.inra.fr>
+#
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
+#
 class galaxy-roles-profiles::role::basic(){
-  class{ 'galaxy-role-profiles::profile::common': }
+  include galaxy-roles-profiles::profile::common
 }
-
