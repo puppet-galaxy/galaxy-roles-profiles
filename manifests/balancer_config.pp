5740933c5003d26d14c7a8ccffeec3833a884ae0
root <root@debian-puppet.local>
2014-06-18 12:59:48 +0000

    First commit.

--- manifests/balancer_config.pp
+++ manifests/balancer_config.pp
@@ -1,10 +1,36 @@
-define galaxy-roles-profiles::balancer_config(
-  $directory = /etc/apache2/conf.d/
-  $balancer_name = 'galaxy'
-  $webworker_starting_port_number= 8000
+# == Class: galaxy-roles-profiles::profile::base
+#
+# This class write the file balancer_galaxy.conf with all the balancer members.
+#
+# === Parameters
+#
+# [*$directory*]: where we want to write balancer_galaxy.conf
+# [*$webworker_starting_port_number*]: port for the first web worker
+#
+# Parameters set by Hiera:
+#
+# [*galaxy-roles-profiles::number_of_workers*]
+# Set the number of web workers
+#
+#==== Examples
+#
+# include galaxy-roles-profiles::balancer_config
+#
+# === Authors
+#
+# S. Bridel <sbridel@versailles.inra.fr>
+# O. Inizan <oinizan@versailles.inra.fr>
+#
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
+#
+class galaxy-roles-profiles::balancer_config(
+  $directory = '/etc/apache2/conf.d/',
+  $webworker_starting_port_number= 8000,
 ){
-  $number_of_web_workers_array = range("0", -1+hiera('galaxy-roles-profiles::number_of_workers'))
+  $number_of_web_workers_array = range('0', -1+hiera('galaxy-roles-profiles::number_of_workers'))
   file { "$directory/balancer_galaxy.conf":
-    content => template("galaxy-roles-profiles/balancer_galaxy.conf.erb"),
+    content => template('galaxy-roles-profiles/balancer_galaxy.conf.erb'),
   }
 }
