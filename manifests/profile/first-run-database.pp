5c4f1a8841536c2f06262971875d39e607bc5846
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-06-27 10:03:53 +0000

    Refactoring.

--- manifests/profile/first-run-database.pp
+++ manifests/profile/first-run-database.pp
@@ -17,42 +17,41 @@
 class galaxy-roles-profiles::profile:multicore-database{
   $directory=$galaxy::params::directory
   galaxy::universe{ 'production':
-      directory                    => hiera('galaxy_roles_profiles::profile::app_directory'),
-      id_secret                    => hiera('galaxy_roles_profiles::profile::id_secret'),
-      db_config                    => true,
-      db_driver                    => hiera('galaxy_roles_profiles::profile::db_driver'),
-      db_host                      => hiera('galaxy_roles_profiles::profile::db_host'),
-      db_port                      => hiera('galaxy_roles_profiles::profile::db_port'),
-      db_opts_pool_size            => hiera('galaxy_roles_profiles::profile:db_opts_pool_size'),
-      db_opts_max_overflow         => hiera('galaxy_roles_profiles::profile::db_opts_max_overflow'),
-      db_username                  => hiera('galaxy_roles_profiles::profile::db_username'),
-      db_password                  => hiera('galaxy_roles_profiles::profile::db_password'),
-      db_database                  => hiera('galaxy_roles_profiles::profile::db_database'),
+      directory            => hiera('galaxy_roles_profiles::profile::app_directory'),
+      id_secret            => hiera('galaxy_roles_profiles::profile::id_secret'),
+      db_config            => true,
+      db_driver            => hiera('galaxy_roles_profiles::profile::db_driver'),
+      db_host              => hiera('galaxy_roles_profiles::profile::db_host'),
+      db_port              => hiera('galaxy_roles_profiles::profile::db_port'),
+      db_opts_pool_size    => hiera('galaxy_roles_profiles::profile:db_opts_pool_size'),
+      db_opts_max_overflow => hiera('galaxy_roles_profiles::profile::db_opts_max_overflow'),
+      db_username          => hiera('galaxy_roles_profiles::profile::db_username'),
+      db_password          => hiera('galaxy_roles_profiles::profile::db_password'),
+      db_database          => hiera('galaxy_roles_profiles::profile::db_database'),
   }->
   exec{'Start Galaxy':
-    path => '/usr/bin:/usr/sbin:/bin:/sbin',
-    cwd => $directory,
-    user => 'galaxy',
-    command => "bash run.sh --daemon",
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd     => $directory,
+    user    => 'galaxy',
+    command => 'bash run.sh --daemon',
     timeout => 0,
     creates => "$directory/first_run_db_done",
     }->
     exec { 'Finish first run':
-      path => '/usr/bin:/usr/sbin:/bin:/sbin',
-      onlyif => "test -f $directory/paster.pid",      
+      path    => '/usr/bin:/usr/sbin:/bin:/sbin',
       command => "sleep 30 |grep serving $directory/paster.log;",
-      tries => 100,
+      tries   => 100,
       returns => 0,
     }->
     exec { 'Stop Galaxy':
-      path => '/usr/bin:/usr/sbin:/bin:/sbin',
-      cwd => $directory,
-      user => 'galaxy',
-      onlyif => "test -f $directory/paster.pid",
-      command => "bash run.sh --stop-daemon",
+      path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+      cwd     => $directory,
+      user    => 'galaxy',
+      onlyif  => "test -f $directory/paster.pid",
+      command => 'bash run.sh --stop-daemon',
     }->
     file { "$directory/first_run_done":
       ensure  => present,
-      content => ' This file is created once the first run of Galaxy with a database like postgresql is finished.",
-    } 
+      content => 'This file is created once the first run of Galaxy with a database like postgresql is finished.',
+    }
 }
