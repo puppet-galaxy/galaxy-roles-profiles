642efcf3011e0cea2e64b9b47cae163681f5570a
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-06-30 07:51:21 +0000

    Add require_login=>false.

--- manifests/profile/multicore-database.pp
+++ manifests/profile/multicore-database.pp
@@ -12,8 +12,7 @@
 #
 # Copyright 2014, unless otherwise noted.
 #
-class galaxy-roles-profiles::profile:multicore-database{
-
+class galaxy_roles_profiles::profile::multicore-database{
 galaxy::toolshed_conf{ 'production':
       directory        => hiera('galaxy_roles_profiles::profile::app_directory'),
       custom_toolsheds => hiera('galaxy_roles_profiles::profile::custom_toolsheds'),
@@ -24,17 +23,18 @@ galaxy::toolshed_conf{ 'production':
     galaxy::universe{ 'production':
       directory                    => hiera('galaxy_roles_profiles::profile::app_directory'),
       id_secret                    => hiera('galaxy_roles_profiles::profile::id_secret'),
+      require_login                => false,
       wk_config                    => true,
       db_config                    => true,
-      db_driver                    => hiera('galaxy_roles_profiles::profile::common::db_driver'),
-      db_host                      => hiera('galaxy_roles_profiles::profile::common::db_host'),
-      db_port                      => hiera('galaxy_roles_profiles::profile::common::db_port'),
-      db_opts_pool_size            => hiera('galaxy_roles_profiles::profile::common::db_opts_pool_size'),
-      db_opts_max_overflow         => hiera('galaxy_roles_profiles::profile::common::db_opts_max_overflow'),
+      db_driver                    => hiera('galaxy_roles_profiles::profile::db_driver'),
+      db_host                      => hiera('galaxy_roles_profiles::profile::db_host'),
+      db_port                      => hiera('galaxy_roles_profiles::profile::db_port'),
+      db_opts_pool_size            => hiera('galaxy_roles_profiles::profile::db_opts_pool_size'),
+      db_opts_max_overflow         => hiera('galaxy_roles_profiles::profile::db_opts_max_overflow'),
       db_username                  => hiera('galaxy_roles_profiles::profile::db_username'),
       db_password                  => hiera('galaxy_roles_profiles::profile::db_password'),
       db_database                  => hiera ('galaxy_roles_profiles::profile::db_database'),
-      number_of_background_workers => hiera('galaxy_roles_profiles::profile::common::number_of_handlers'),
+      number_of_background_workers => hiera('galaxy_roles_profiles::profile::number_of_handlers'),
       number_of_web_workers        => hiera('galaxy_roles_profiles::number_of_workers'),
     }
 }
