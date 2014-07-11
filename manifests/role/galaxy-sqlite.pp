2ec4ad3c0194bea4719fab67425074e83bcda194
sbridel <sebastien.bridel@versailles.inra.fr>
2014-07-11 08:08:34 +0000

    First Commit.

---
+++ manifests/role/galaxy-sqlite.pp
@@ -0,0 +1,22 @@
+# == Class galaxy-roles-profiles::role::basic
+# This role create one instance of galaxy with basic configuration.
+# That means Galaxy runs with only one core and with SQLite.
+#
+# === Example
+#
+# include galaxy-roles-profiles::role::basic
+#
+# === Authors
+#
+# O. Inizan <oinizan@versailles.inra.fr>
+# S. Bridel <sbridel@versailles.inra.fr>
+# M. Loaec <mloaec@versailles.inra.fr>
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
+class galaxy_roles_profiles::role::galaxy-sqlite{
+  class { 'galaxy_roles_profiles::profile::common':
+  }->
+  class { 'galaxy_roles_profiles::profile::sqlite':
+  }
+}
