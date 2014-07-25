68c1844380ea0a8f024af4dd0bbc64fef94e7cc1
Eric Rasche <rasche.eric@yandex.ru>
2014-07-25 16:29:50 -0500

    Migrated galaxy code in common into correct module

--- manifests/profile/common.pp
+++ manifests/profile/common.pp
@@ -1,10 +1,2 @@
 class galaxy_roles_profiles::profile::common {
-  include ::galaxy
-  include galaxy::install
-  include galaxy::universe
-  include galaxy::toolshed_conf
-  if $galaxy::universe::wk_config == true  {
-    include galaxy::job_conf
-  }
-  include galaxy::common_startup
 }
--- manifests/profile/galaxy.pp
+++ manifests/profile/galaxy.pp
@@ -1,10 +1,14 @@
-#
-#
-#
-#
-#
-#
-#
 class galaxy_roles_profiles::profile::galaxy{
+  include ::galaxy
+  include galaxy::install
+  class { "galaxy::universe":
+    id_secret => hiera("galaxy::universe::id_secret"),
+    require_login => hiera("galaxy::universe::require_login"),
+  }
+  include galaxy::toolshed_conf
+  if $galaxy::universe::wk_config == true  {
+    include galaxy::job_conf
+  }
+  include galaxy::common_startup
   include galaxy::service
 }
