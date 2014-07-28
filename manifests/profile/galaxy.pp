57afd144cd5d1369d4a7e801bbde8aa738214742
Eric Rasche <rasche.eric@yandex.ru>
2014-07-28 10:18:55 -0500

    Reworked dependencies

--- manifests/profile/galaxy.pp
+++ manifests/profile/galaxy.pp
@@ -1,15 +1,16 @@
 class galaxy_roles_profiles::profile::galaxy{
   include ::galaxy
-  include galaxy::install
+  class { "galaxy::dependencies":
+  } ->
+  class { "galaxy::install":
+  } ->
   class { "galaxy::universe":
     id_secret => hiera("galaxy::universe::id_secret"),
     require_login => hiera("galaxy::universe::require_login"),
-  }
-  include galaxy::toolshed_conf
-  if $galaxy::universe::wk_config == true  {
-    include galaxy::job_conf
-  }
-  class { "galaxy::first_run":
+  } ->
+  class { "galaxy::toolshed_conf":
+  } ->
+  class { "galaxy::job_conf":
   } ->
   class { "galaxy::service":
   }
