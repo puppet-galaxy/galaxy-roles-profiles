d27e30b0cec5ce74cf9676edcc0998fb33e3f8f7
sbridel <sebastien.bridel@versailles.inra.fr>
2014-08-13 14:49:44 +0200

    Add test

--- scripts/onecore-run.sh
+++ scripts/onecore-run.sh
@@ -32,3 +32,45 @@ IPVM_array[2]=$(sudo docker inspect galaxy-po | grep IPAddress | cut -d\" -f4)
 ##ROLE GALAXY-POSTGRESQL-APACHE
 sudo docker run -d --name galaxy-apo -e "ROLE=galaxy-apache-postgresql" -e "GLOG=paster.log" galaxy/opostgres:$GITLOG
 IPVM_array[3]=$(sudo docker inspect galaxy-apo | grep IPAddress | cut -d\" -f4)
+
+################
+####        ####
+##### TEST #####
+####        ####
+################
+
+reportGalaxyService()
+{
+    if [ $(curl $1:$2 | grep -c welcome ) -ge 1 ]
+        then
+        echo $1":"$2"is working" >> $DIRVM/rapport-$GITLOG
+    else
+        echo $1":"$2"is broken" >> $DIRVM/rapport-$GITLOG
+    fi
+}
+#Attendre que tout soit démarrer
+sleep 5m
+
+for i in IPVM_array
+    do
+        if [i eq 0]
+            then
+            echo "<< GALAXY-SQLITE Onecore >>" >> $DIRVM/rapport-$GITLOG
+            reportGalaxyService ${IPVM[i]} 8080
+        elif [i eq 1]
+            then
+            echo "<< GALAXY-APACHE-SQLITE Onecore >>" >> $DIRVM/rapport-$GITLOG
+            reportGalaxyService ${IPVM[i]} 8080
+            reportGalaxyService ${IPVM[i]} 8081
+        elif [i eq 2]
+            then
+            echo "<< GALAXY-POSTGRESQL Onecore >>" >> $DIRVM/rapport-$GITLOG
+            reportGalaxyService ${IPVM[i]} 8080
+        else
+            then
+            echo "<< GALAXY-APACHE-POSTGRESQL Onecore >>" >> $DIRVM/rapport-$GITLOG
+            reportGalaxyService ${IPVM[i]} 8080
+            reportGalaxyService ${IPVM[i]} 8081
+        fi
+done
+exit 0
